9c21ab5739b335dabca0ffa4208d15af98390588
chris-pidgeon <cpidgeon@yahoo.com>
2025-11-10 09:47:42 +0800

    fixed variables

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-RgName=`LB-FrontendAccess-Scenario`
-Location=`australiaeast`
+RgName="LB-FrontendAccess-Scenario"
+Location="australiaeast"
 
 date
 # Create a Virtual Network for the VMs
