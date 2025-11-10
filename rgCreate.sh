bfd971d3283c3763fbb1058bcd33fc0689325bc7
chris-pidgeon <cpidgeon@yahoo.com>
2025-11-10 10:02:30 +0800

    now?

--- rgCreate.sh
+++ rgCreate.sh
@@ -3,5 +3,4 @@
 RgName="LB-FrontendAccess-Scenario"
 Location="australiaeast"
 
-az group create --location $Location
-                --name $RgName
+az group create --location $Location --name $RgName
